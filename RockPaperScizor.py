547f37eb1e3aba6f5ffb2dee598381b5f8447407
turtle1234543 <52340798+turtle1234543@users.noreply.github.com>
2020-01-22 16:51:35 -0800

    Add files via upload

---
+++ RockPaperScizor.py
@@ -0,0 +1,35 @@
+import random
+#Bot and Human Select Weapon
+user=input(print("Rock"" Paper"" Scissors"))
+robo=random.choice(["Rock", "Paper", "Scissors"])
+Rock="Rock"
+Paper="Paper"
+Scissors="Scissors"
+print("The human has selected "+user +"\nThe Robo has selected "+robo)
+#loop
+y='yes'
+n='no'
+while 
+if(user=="Rock"and robo=="Paper"):
+    print("Robo wins")
+if(user=="Paper" and robo=="Scissors"):
+    print("Robo wins")
+if(user=="Scissors" and robo=="Rock"):
+    print("Robo wins")
+
+if(user=="Rock" and robo=="Scissors"):
+    print("Human Wins")
+if(user=="Paper" and robo=="Rock"):
+    print("Human Wins")
+if(user=="Scissors" and robo=="Paper"):
+    print("Human Wins")
+
+if(user=="Rock" and robo=="Rock"):
+    print("Draw")
+if(user=="Paper" and robo=="Paper"):
+    print("Draw")
+if(user=="Scissors" and robo=="Scissors"):
+    print("Draw")
+loop = input("continue? y/n")
+while(loop=="y")
+user=input
